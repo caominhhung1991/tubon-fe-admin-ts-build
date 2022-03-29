9fa41222280924403e13b653a90cd1e4c94bba4e
Hung Cao <hungcao@Hungs-MacBook-Pro.local>
2022-03-29 11:59:05 +0700

    update

--- static/js/54.b1e3ce37.chunk.js
+++
@@ -1,1 +0,0 @@
-(this["webpackJsonptu-bon"]=this["webpackJsonptu-bon"]||[]).push([[54],{928:function(t,c,n){"use strict";n.r(c);var e=n(0),b=n.n(e),u=n(45),r=n(56),f=n(16),i=n(350),o=n(175),s=n(17);c.default=function(){var t=Object(r.b)();return b.a.useEffect((function(){return t(Object(i.d)()),t(Object(o.h)()),function(){Object(f.f)()}}),[]),Object(s.b)(s.a,{children:Object(s.b)(u.c,{})})}}}]);
